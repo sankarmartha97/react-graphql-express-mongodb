2f3176722d227395f50972fd7722e5f561e9acfa
Sankar Martha <sankarmartha.123@gmail.com>
2020-07-15 20:28:27 +0530

    added graphiql platform and fix bug of schema and query required

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 // const graphqlHTTP = require('express-graphql');
 // const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const { graphqlHTTP } = require('express-graphql');
-
+const schema = require('./schema/scema');
 
 const app = express();
 
 //graphqlHTTP use for finding graphql 
 app.use('/graphql',graphqlHTTP({
-
+    schema:schema,
+    graphiql:true
 }));
 
 app.listen(4000,() => {
